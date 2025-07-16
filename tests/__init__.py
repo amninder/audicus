c7030a4106da2997f8422727881ba9c4831a0e8a
amninder-narota <163357823+amninder-narota@users.noreply.github.com>
2025-07-16 16:16:02 -0400

    defines test client

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,21 @@
 import unittest
-import flask
 from audicus.models.order import Order
 from audicus.models.subscription import Subscription
 from audicus.models.db import (db)
+from audicus.app import app
 
 
 class BaseTest(unittest.TestCase):
 
     def setUp(self):
-        self.app = flask.Flask(__name__)
+        self.app = app
+        self.app.testing = True
         self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
         self.app.config["SQLALCHEMY_ECHO"] = False
         self.app.config["TESTING"] = True
         self.app_context = self.app.app_context()
         self.app_context.push()
+        self.client = self.app.test_client()
 
         self.db = db
         self.db.init_app(self.app)
